7c0fffe8fee6a02da683c02ee1b8bf2fe30b7a38
aitounejjar <mouhcine@stanford.edu>
2017-04-09 10:36:51 -0400

    - inverted indexes are implemented for each block, but they're not yet merged to a single inv. index - added an indexing utility classe that has 2 methods so far:   + one for merging two posting lists   + one for merging two inverted indexes

--- src/cs276/assignments/Index.java
+++ src/cs276/assignments/Index.java
@@ -1,5 +1,7 @@
 package cs276.assignments;
 
+//import cs276.util.TermIdDocIdPairComparator;
+
 import cs276.util.Pair;
 
 import java.io.BufferedReader;
@@ -11,213 +13,300 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Index {
 
-	// Term id -> (position in index file, doc frequency) dictionary
-	private static Map<Integer, Pair<Long, Integer>> postingDict 
-		= new TreeMap<Integer, Pair<Long, Integer>>();
-	// Doc name -> doc id dictionary
-	private static Map<String, Integer> docDict
-		= new TreeMap<String, Integer>();
-	// Term -> term id dictionary
-	private static Map<String, Integer> termDict
-		= new TreeMap<String, Integer>();
-	// Block queue
-	private static LinkedList<File> blockQueue
-		= new LinkedList<File>();
+    // Term id -> (position in index file, doc frequency) dictionary
+    private static Map<Integer, Pair<Long, Integer>> postingDict = new TreeMap<Integer, Pair<Long, Integer>>();
 
-	// Total file counter
-	private static int totalFileCount = 0;
-	// Document counter
-	private static int docIdCounter = 0;
-	// Term counter
-	private static int wordIdCounter = 0;
-	// Index
-	private static BaseIndex index = null;
+    // Doc name -> doc id dictionary
+    private static Map<String, Integer> docDict = new TreeMap<String, Integer>();
+    private static Map<Integer, String> docDict_reversed = new TreeMap<>();
 
-	
-	/* 
-	 * Write a posting list to the given file 
-	 * You should record the file position of this posting list
-	 * so that you can read it back during retrieval
-	 * 
-	 * */
-	private static void writePosting(FileChannel fc, PostingList posting)
-			throws IOException {
+    // Term -> term id dictionary. This makes index construction more efficient. How ?
+    private static Map<String, Integer> termDict = new TreeMap<String, Integer>();
+    private static Map<Integer, String> termDict_reversed = new TreeMap<>();
+
+    // Block queue
+    private static LinkedList<File> blockQueue = new LinkedList<File>();
+
+    // Total file counter
+    private static int totalFileCount = 0;
+
+    // Document counter
+    private static int docIdCounter = 0;
+
+    // Term counter
+    private static int wordIdCounter = 0;
+
+    // Index
+    private static BaseIndex index = null;
+
+
+    /*
+     * Write a posting list to the given file
+     * You should record the file position of this posting list
+     * so that you can read it back during retrieval
+     *
+     * */
+    private static void writePosting(FileChannel fc, PostingList posting) throws IOException {
 		/*
 		 * TODO: Your code here
-		 *	 
+		 *
 		 */
-	}
 
-	public static void main(String[] args) throws IOException {
+        try {
+            index.writePosting(fc, posting);
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
 		/* Parse command line */
-		if (args.length != 3) {
-			System.err
-					.println("Usage: java Index [Basic|VB|Gamma] data_dir output_dir");
-			return;
-		}
+        if (args.length != 3) {
+            System.err.println("Usage: java Index [Basic|VB|Gamma] data_dir output_dir");
+            return;
+        }
 
 		/* Get index */
-		String className = "cs276.assignments." + args[0] + "Index";
-		try {
-			Class<?> indexClass = Class.forName(className);
-			index = (BaseIndex) indexClass.newInstance();
-		} catch (Exception e) {
-			System.err
-					.println("Index method must be \"Basic\", \"VB\", or \"Gamma\"");
-			throw new RuntimeException(e);
-		}
+        String className = "cs276.assignments." + args[0] + "Index";
+        try {
+            Class<?> indexClass = Class.forName(className);
+            index = (BaseIndex) indexClass.newInstance();
+        } catch (Exception e) {
+            System.err.println("Index method must be \"Basic\", \"VB\", or \"Gamma\"");
+            throw new RuntimeException(e);
+        }
 
 		/* Get root directory */
-		String root = args[1];
-		File rootdir = new File(root);
-		if (!rootdir.exists() || !rootdir.isDirectory()) {
-			System.err.println("Invalid data directory: " + root);
-			return;
-		}
+        String root = args[1];
+        File rootdir = new File(root);
+        if (!rootdir.exists() || !rootdir.isDirectory()) {
+            System.err.println("Invalid data directory: " + root);
+            return;
+        }
 
 		/* Get output directory */
-		String output = args[2];
-		File outdir = new File(output);
-		if (outdir.exists() && !outdir.isDirectory()) {
-			System.err.println("Invalid output directory: " + output);
-			return;
-		}
+        String output = args[2];
 
-		if (!outdir.exists()) {
-			if (!outdir.mkdirs()) {
-				System.err.println("Create output directory failure");
-				return;
-			}
-		}
+        File outdir = new File(output);
+        deleteDirectory(outdir);
+
+        if (outdir.exists() && !outdir.isDirectory()) {
+            System.err.println("Invalid output directory: " + output);
+            return;
+        }
+
+        if (!outdir.exists()) {
+            if (!outdir.mkdirs()) {
+                System.err.println("Create output directory failure");
+                return;
+            }
+        }
 
 		/* A filter to get rid of all files starting with .*/
-		FileFilter filter = new FileFilter() {
-			@Override
-			public boolean accept(File pathname) {
-				String name = pathname.getName();
-				return !name.startsWith(".");
-			}
-		};
+        FileFilter filter = new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                String name = pathname.getName();
+                return !name.startsWith(".");
+            }
+        };
 
 		/* BSBI indexing algorithm */
-		File[] dirlist = rootdir.listFiles(filter);
 
-		/* For each block */
-		for (File block : dirlist) {
-			File blockFile = new File(output, block.getName());
-			blockQueue.add(blockFile);
+        // get the list of files under that the rootdir (i.e. the data_dir)
+        File[] dirlist = rootdir.listFiles(filter);
 
-			File blockDir = new File(root, block.getName());
-			File[] filelist = blockDir.listFiles(filter);
-			
+        //--------------------------------------------------------------------------------------------------------------
+        // create, and write to disk, an inverted index for each of the blocks
+        //--------------------------------------------------------------------------------------------------------------
+
+		/* For each block - each subdirectory is treated as a block in our cases */
+        for (File block : dirlist) {
+
+            // keys are termIds, values are posting lists
+            Map<Integer, PostingList> map = new TreeMap<>();
+
+            File blockFile = new File(output, block.getName());
+            blockQueue.add(blockFile);
+
+            File blockDir = new File(root, block.getName());
+            File[] filelist = blockDir.listFiles(filter);
+
 			/* For each file */
-			for (File file : filelist) {
-				++totalFileCount;
-				String fileName = block.getName() + "/" + file.getName();
-				docDict.put(fileName, docIdCounter++);
-				
-				BufferedReader reader = new BufferedReader(new FileReader(file));
-				String line;
-				while ((line = reader.readLine()) != null) {
-					String[] tokens = line.trim().split("\\s+");
-					for (String token : tokens) {
-						/*
+            for (File file : filelist) {
+                ++totalFileCount;
+                String fileName = block.getName() + "/" + file.getName();
+                docDict.put(fileName, ++docIdCounter);
+                docDict_reversed.put(docIdCounter, fileName);
+
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] tokens = line.trim().split("\\s+");
+                    for (String token : tokens) {
+
+					    /*
 						 * TODO: Your code here
 						 *       For each term, build up a list of
 						 *       documents in which the term occurs
 						 */
-					}
-				}
-				reader.close();
-			}
+
+
+                        int termId;
+                        if (!termDict.containsKey(token)) {
+                            // add the new term to dictionary
+                            termDict.put(token, ++wordIdCounter);
+                            termDict_reversed.put(wordIdCounter, token);
+                            // assign it an empty postings list
+                            map.put(wordIdCounter, new PostingList(wordIdCounter));
+                            termId = wordIdCounter;
+                        } else {
+                            termId = termDict.get(token);
+                            if (map.get(termId) == null) {
+                                // this term already exists in the dictionary, but this is the first time it was discovered in this block
+                                map.put(termId, new PostingList(termId));
+                            }
+                        }
+
+                        List<Integer> docs = map.get(termId).getList();
+                        if (!docs.contains(docIdCounter)) {
+                            docs.add(docIdCounter);
+                        }
+
+                        map.put(termId, new PostingList(termId, docs));
+
+                    }
+                }
+                reader.close();
+            } // end - for each file
 
 			/* Sort and output */
-			if (!blockFile.createNewFile()) {
-				System.err.println("Create new block failure.");
-				return;
-			}
-			
-			RandomAccessFile bfc = new RandomAccessFile(blockFile, "rw");
-			
+            if (!blockFile.createNewFile()) {
+                System.err.println("Create new block failure.");
+                return;
+            }
+
+            RandomAccessFile bfc = new RandomAccessFile(blockFile, "rw");
+
 			/*
 			 * TODO: Your code here
-			 *       Write all posting lists for all terms to file (bfc) 
+			 *       Write all posting lists for all terms to file (bfc)
 			 */
-			
-			bfc.close();
-		}
+
+            for( int tId : map.keySet() ) {
+                writePosting(bfc.getChannel(), map.get(tId));
+            }
+
+            bfc.close();
+        } /* end - for each block
+
+		// by now, for each each block an inverted index has been constructed and written to disk
+		// next step, and final one, is to merge all of these inverted indexes into a single one
+
+		//--------------------------------------------------------------------------------------------------------------
+        // merge the many inverted indexes into a single one
+        //--------------------------------------------------------------------------------------------------------------
 
 		/* Required: output total number of files. */
-		System.out.println(totalFileCount);
+        System.out.println(totalFileCount);
 
 		/* Merge blocks */
-		while (true) {
-			if (blockQueue.size() <= 1)
-				break;
+        while (true) {
+            if (blockQueue.size() <= 1)
+                break;
 
-			File b1 = blockQueue.removeFirst();
-			File b2 = blockQueue.removeFirst();
-			
-			File combfile = new File(output, b1.getName() + "+" + b2.getName());
-			if (!combfile.createNewFile()) {
-				System.err.println("Create new block failure.");
-				return;
-			}
+            File b1 = blockQueue.removeFirst();
+            File b2 = blockQueue.removeFirst();
 
-			RandomAccessFile bf1 = new RandomAccessFile(b1, "r");
-			RandomAccessFile bf2 = new RandomAccessFile(b2, "r");
-			RandomAccessFile mf = new RandomAccessFile(combfile, "rw");
-			 
+            File combfile = new File(output, b1.getName() + "+" + b2.getName());
+            if (!combfile.createNewFile()) {
+                System.err.println("Create new block failure.");
+                return;
+            }
+
+            RandomAccessFile bf1 = new RandomAccessFile(b1, "r");
+            RandomAccessFile bf2 = new RandomAccessFile(b2, "r");
+            RandomAccessFile mf = new RandomAccessFile(combfile, "rw");
+
 			/*
 			 * TODO: Your code here
 			 *       Combine blocks bf1 and bf2 into our combined file, mf
 			 *       You will want to consider in what order to merge
 			 *       the two blocks (based on term ID, perhaps?).
-			 *       
+			 *
 			 */
-			
-			bf1.close();
-			bf2.close();
-			mf.close();
-			b1.delete();
-			b2.delete();
-			blockQueue.add(combfile);
-		}
+
+
+            try {
+                PostingList pl1 = index.readPosting(bf1.getChannel());
+                PostingList pl2 = index.readPosting(bf2.getChannel());
+                int i=0;
+            } catch (Throwable ex) {
+                ex.printStackTrace();
+            }
+
+
+            bf1.close();
+            bf2.close();
+            mf.close();
+            b1.delete();
+            b2.delete();
+
+            blockQueue.add(combfile);
+        }
 
 		/* Dump constructed index back into file system */
-		File indexFile = blockQueue.removeFirst();
-		indexFile.renameTo(new File(output, "corpus.index"));
+        File indexFile = blockQueue.removeFirst();
+        indexFile.renameTo(new File(output, "corpus.index"));
 
-		BufferedWriter termWriter = new BufferedWriter(new FileWriter(new File(
-				output, "term.dict")));
-		for (String term : termDict.keySet()) {
-			termWriter.write(term + "\t" + termDict.get(term) + "\n");
-		}
-		termWriter.close();
+        BufferedWriter termWriter = new BufferedWriter(new FileWriter(new File(output, "term.dict")));
+        for (String term : termDict.keySet()) {
+            termWriter.write(term + "\t" + termDict.get(term) + "\n");
+        }
+        termWriter.close();
 
-		BufferedWriter docWriter = new BufferedWriter(new FileWriter(new File(
-				output, "doc.dict")));
-		for (String doc : docDict.keySet()) {
-			docWriter.write(doc + "\t" + docDict.get(doc) + "\n");
-		}
-		docWriter.close();
+        BufferedWriter docWriter = new BufferedWriter(new FileWriter(new File(output, "doc.dict")));
+        for (String doc : docDict.keySet()) {
+            docWriter.write(doc + "\t" + docDict.get(doc) + "\n");
+        }
+        docWriter.close();
 
-		BufferedWriter postWriter = new BufferedWriter(new FileWriter(new File(
-				output, "posting.dict")));
-		for (Integer termId : postingDict.keySet()) {
-			postWriter.write(termId + "\t" + postingDict.get(termId).getFirst()
-					+ "\t" + postingDict.get(termId).getSecond() + "\n");
-		}
-		postWriter.close();
-	}
+        BufferedWriter postWriter = new BufferedWriter(new FileWriter(new File(output, "posting.dict")));
+        for (Integer termId : postingDict.keySet()) {
+            postWriter.write(termId + "\t" + postingDict.get(termId).getFirst() + "\t" + postingDict.get(termId).getSecond() + "\n");
+        }
+        postWriter.close();
+    }
+
+    //--------------------------------------------------------------------------------------------------------------
+    // private method(s)
+    //--------------------------------------------------------------------------------------------------------------
+
+    private static boolean deleteDirectory(File directory) {
+        if(directory.exists()){
+            File[] files = directory.listFiles();
+            if(null!=files){
+                for(int i=0; i<files.length; i++) {
+                    if(files[i].isDirectory()) {
+                        deleteDirectory(files[i]);
+                    }
+                    else {
+                        files[i].delete();
+                    }
+                }
+            }
+        }
+        return(directory.delete());
+    }
 
 }
---
+++ src/cs276/util/IndexUtils.java
@@ -0,0 +1,110 @@
+package cs276.util;
+
+import cs276.assignments.PostingList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A utility class that holds various methods used by the indexing algorithms
+ */
+
+public class IndexUtils {
+
+    public static List<Integer> mergePostingLists(List<Integer> list1, List<Integer> list2) {
+        // TODO: implement the algorithm that merges two posting lists into a single one
+
+        // Assumption(s)
+        // - both posting lists come from two different inverted indexes, hence docIds are unique
+
+        List<Integer> result = new ArrayList<>();
+        result.addAll(list1);
+        result.addAll(list2);
+        return result;
+    }
+
+    /** Returns the next item from the Iterator, or null if it is exhausted.
+     *  (This is a more C-like method than idiomatic Java, but we use it so as
+     *  to be more parallel to the pseudo-code in the textbook.)
+     */
+
+    public static List<PostingList> mergeInvertedIndexes(List<PostingList> index1, List<PostingList> index2) {
+        // resulting inverted index
+        List<PostingList> result = new ArrayList<>();
+
+        int i=0, j=0;
+
+        int I_MAX = index1.size() - 1;
+        int J_MAX = index2.size() - 1;
+
+        int t1 = index1.get(i).getTermId();
+        int t2 = index2.get(j).getTermId();
+
+        while (i <= I_MAX || j <= J_MAX) {
+            if (i<=I_MAX) {
+                t1 = index1.get(i).getTermId();
+            }
+            if (j<=J_MAX) {
+                t2 = index2.get(j).getTermId();
+            }
+            // Index1 complete, result filled by index2
+            if (i > I_MAX){
+                result.add(index2.get(j));
+                j++;
+            }else if ( j > J_MAX){
+                result.add(index1.get(i));
+                i++;
+            }else if (t1 == t2) {
+                List<Integer> merged = mergePostingLists(index1.get(i).getList(), index2.get(j).getList());
+                PostingList list = new PostingList(t1, merged);
+                result.add(list);
+                i++;
+                j++;
+            }else if (t1 < t2) {
+                result.add(index1.get(i));
+                i++;
+            } else if (t1 > t2) {
+                result.add(index2.get(j));
+                j++;
+            }else {
+                // Nothing should be here
+            }
+
+        }
+
+        return result;
+    }
+
+    private void move_this_method_to_a_jUnit() {
+
+        List<PostingList> i1 = new ArrayList<>();
+        List<PostingList> i2 = new ArrayList<>();
+
+        // index 1
+        PostingList p2 = new PostingList(2, Arrays.asList(new Integer[]{1,2,7,8}));
+        PostingList p31 = new PostingList(3, Arrays.asList(new Integer[]{5,7,9}));
+        PostingList p1 = new PostingList(1, Arrays.asList(new Integer[]{1,6,13,15}));
+        PostingList p9 = new PostingList(9, Arrays.asList(new Integer[]{1,6,13,25}));
+
+        // index 2
+        PostingList p32 = new PostingList(3, Arrays.asList(new Integer[]{17,19,21,33}));
+        PostingList p5 = new PostingList(5, Arrays.asList(new Integer[]{24,28,29,100}));
+        PostingList p7 = new PostingList(7, Arrays.asList(new Integer[]{16,17,18,19}));
+
+
+        i1.add(p2);
+        i1.add(p31);
+        i1.add(p1);
+        i1.add(p9);
+
+        i2.add(p32);
+        i2.add(p5);
+        i2.add(p7);
+
+        List<PostingList> result = IndexUtils.mergeInvertedIndexes(i1, i2);
+        // TBD: add some verification on the result
+        // TBD: move method to a jUnit
+    }
+
+}
